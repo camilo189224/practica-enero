c4ea76991913a352e09b9e6748e267e254740653
Your Name <you@example.com>
2023-01-20 20:57:45 -0500

    se crea timertask para el inicio de la app

--- app/src/main/java/com/example/turistiando/Splash.java
+++ app/src/main/java/com/example/turistiando/Splash.java
@@ -1,13 +1,48 @@
 package com.example.turistiando;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Splash extends AppCompatActivity {
+import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class Splash extends AppCompatActivity {
+    //atributos
+    MediaPlayer cancion;
+
+
+    //metodos
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+
+        //oculte el action bar
+        ActionBar barra = getSupportActionBar();
+        barra.hide();
+
+        //Lanzar la cancion
+        cancion = MediaPlayer.create(this,R.raw.audio);
+        cancion.start();
+
+
+        //Temporizar la durecion del splash
+        TimerTask inicioapp = new TimerTask() {
+            @Override
+            public void run() {
+
+                //Lanzar una nueva actividad
+                Intent intent = new Intent(Splash.this,MainActivity.class);
+                startActivity(intent);
+            }
+        };
+
+        Timer tiempo = new Timer();
+        tiempo.schedule(inicioapp,100000);
     }
 }
